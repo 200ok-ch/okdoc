20e0369f98d6f201a236500d39675a2ac4a75e53
Alain M. Lafon <alain@200ok.ch>
2020-10-12 12:01:59 +0200

    fix: Do not rename `,`
         Renaming `,` to `_` conflicts with the `tags` functionality. When
         setting multiple tags separated by commas, these files would get
         renamed when running `make rename`.

--- rename.rb
+++ rename.rb
@@ -2,6 +2,6 @@
 # coding: utf-8
 
 Dir.glob('**/*.pdf').each do |file|
-  renamed = file.gsub(%r{[^A-Za-z0-9._\-\/äüöÄÜÖ]}, '_')
+  renamed = file.gsub(%r{[^A-Za-z0-9.,_\-\/äüöÄÜÖ]}, '_')
   puts 'mv "%s" "%s"' % [file, renamed] if renamed != file
 end
