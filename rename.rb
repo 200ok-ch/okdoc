34475d70f09bf66cf49d65e1ebbc6950a5d417f2
Alain M. Lafon <alain@200ok.ch>
2020-10-15 12:27:18 +0200

    chore: Allow [] in filenames

--- rename.rb
+++ rename.rb
@@ -3,6 +3,6 @@
 
 Dir.glob('**/*.pdf').each do |file|
   # strip filename and replace unwanted characters
-  renamed = "#{File.dirname(file)}/#{File.basename(file, ".pdf").strip.gsub(%r{[^A-Za-z0-9.,_\-\/äüöÄÜÖ]}, '_')}.pdf"
+  renamed = "#{File.dirname(file)}/#{File.basename(file, ".pdf").strip.gsub(%r{[^A-Za-z0-9.,_\[\]\-\/äüöÄÜÖ]}, '_')}.pdf"
   puts 'mv "%s" "%s"' % [file, renamed] if renamed != file
 end
